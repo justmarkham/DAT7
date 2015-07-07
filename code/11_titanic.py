d7be7ad69d0e4b72bdeec9897d2b5e7fc618dea4
Kevin Markham <justmarkham@gmail.com>
2015-07-07 11:45:13 -0400

    add solution to class 11 exercise

---
+++ code/11_titanic.py
@@ -0,0 +1,40 @@
+'''
+EXERCISE: Predicting Survival on the Titanic
+'''
+
+# TASK 1: read the data from titanic.csv into a DataFrame
+import pandas as pd
+titanic = pd.read_csv('titanic.csv', index_col='PassengerId')
+
+# TASK 2: define Pclass/Parch as the features and Survived as the response
+feature_cols = ['Pclass', 'Parch']
+X = titanic[feature_cols]
+y = titanic.Survived
+
+# TASK 3: split the data into training and testing sets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+# TASK 4: fit a logistic regression model and examine the coefficients
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+zip(feature_cols, logreg.coef_[0])
+
+# TASK 5: make predictions on testing set and calculate accuracy
+y_pred_class = logreg.predict(X_test)
+from sklearn import metrics
+print metrics.accuracy_score(y_test, y_pred_class)
+
+# TASK 6 (BONUS): compare testing accuracy to null accuracy
+1 - y_test.mean()
+
+# TASK 7 (BONUS): add Age as a feature and calculate testing accuracy
+titanic.Age.fillna(titanic.Age.mean(), inplace=True)
+feature_cols = ['Pclass', 'Parch', 'Age']
+X = titanic[feature_cols]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+logreg.fit(X_train, y_train)
+zip(feature_cols, logreg.coef_[0])
+y_pred_class = logreg.predict(X_test)
+print metrics.accuracy_score(y_test, y_pred_class)
